5bff14e63be5baf92a672238f116558664ad74e6
Praveen85994 <pchouhan009@gmail.com>
2023-08-09 23:35:41 +0530

    third changes as a local branch

--- Practicegit2/src/test/java/myprogram/Program1.java
+++ Practicegit2/src/test/java/myprogram/Program1.java
@@ -8,6 +8,8 @@ public class Program1 {
 		//some changes in program and to program we have to send in github
 		//some chanes again
 		//changes im doing in github repo pull
+		
+		//doing some changes as a local branch
 	}
 
 }
