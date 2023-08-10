cf059eb75ba20957544c8759247d21f86dd5d0ee
Praveen85994 <pchouhan009@gmail.com>
2023-08-10 11:24:07 +0530

    mmmmm

---
+++ Practicegit2/src/test/java/myprogram/Program1.java
@@ -0,0 +1,20 @@
+package myprogram;
+
+public class Program1 {
+	public static void main(String[] args) {
+		System.out.println("first program");
+		System.out.println("my second changes");
+		System.out.println("my third");
+		//some changes
+		
+		//some changes in program and to program we have to send in github
+		//some chanes again
+		//changes im doing in github repo pull
+		
+		//doing some changes as a local branch
+		//some changes in program push
+		
+		
+	}
+
+}
